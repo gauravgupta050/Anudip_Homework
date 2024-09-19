42bd6fe262a598fdfcb530055a8d3ac453720405
Gaurav Gupta <92253785+gauravgupta050@users.noreply.github.com>
2024-09-19 14:01:08 +0530

    Create ShapeManagement.java

---
+++ Assignment-2/ShapeManagement.java
@@ -0,0 +1,27 @@
+abstract class Shape {
+    abstract void draw();
+}
+
+class Circle extends Shape {
+    @Override
+    void draw() {
+        System.out.println("Drawing a Circle");
+    }
+}
+
+class Rectangle extends Shape {
+    @Override
+    void draw() {
+        System.out.println("Drawing a Rectangle");
+    }
+}
+
+public class ShapeManagement {
+    public static void main(String[] args) {
+        Shape shape1 = new Circle(); 
+        Shape shape2 = new Rectangle();
+      
+        shape1.draw();  
+        shape2.draw();  
+    }
+}
